1c715c3b226e5cef7c224448485b3d1ada30b49a
c1kvoy <seva33039@gmail.com>
2024-08-13 22:29:25 +0700

    PostModel added

--- src/back/models/models.py
+++ src/back/models/models.py
@@ -1,6 +1,6 @@
 from src.back.database.db import Base
 
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 
 class UserModel(Base):
@@ -10,3 +10,19 @@ class UserModel(Base):
     email: Mapped[str] = mapped_column(index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     refresh_token: Mapped[str] = mapped_column(nullable=True)
+
+    posts: Mapped[list["PostModel"]] = relationship("PostModel", back_populates="user")
+
+
+class PostModel(Base):
+    __tablename__ = 'posts'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, nullable=False)
+    title: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[str] = mapped_column()
+    created_at: Mapped[int] = mapped_column(nullable=False)
+
+    user: Mapped["UserModel"] = relationship("UserModel", back_populates="posts")
+
+
+class Like
